31eb3c36f167ab89954ea9e94f4ee70fc4d89023
YazawaKenichi <tonfarken83825@gmail.com>
2023-09-22 23:02:17 +0900

    A ボタン以外は no_accel_decel するようにした

--- raspicat/src/velocity_smoother_controller.cpp
+++ raspicat/src/velocity_smoother_controller.cpp
@@ -146,7 +146,8 @@ void VelocitySmootherController::callbackJoy(
     } else if (shouldExecute(msg->buttons, no_accel_decel_button_)) {
       no_accel_decel();
     } else {
-        stop();
+      no_accel_decel();
+      // stop();
     }
   }
 }
